63e5d757bcb0051b89431d76c7a9f75233cf35f6
saldsj3@gmail.com <saldsj3@gmail.com@7b9d8fdb-5d98-e7cb-5d11-7a05db268fdb>
2012-09-06 02:12:52 +0000

    removed Steel_Batallion_64-MW4 since its functions are now available using regular Steel_Batallion_64-MW4.exe

--- trunk/ steel-batallion-64/Steel_Batallion_64-MW4/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Steel_Batallion_64-MW4.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
