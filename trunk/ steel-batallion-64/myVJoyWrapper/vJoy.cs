aa911f8208b80749aa6c80e0fcadfbe09d024fcc
saldsj3@gmail.com <saldsj3@gmail.com@7b9d8fdb-5d98-e7cb-5d11-7a05db268fdb>
2012-09-13 05:55:17 +0000

    making some progress, creating my own C# wrapper

---
+++ trunk/ steel-batallion-64/myVJoyWrapper/vJoy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace myVJoyWrapper
+{
+
+    enum VjdStat
+    {
+        //status can be one of the following values:
+        VJD_STAT_OWN, // The vJoy Device is owned by this application.
+        VJD_STAT_FREE, // The vJoy Device is NOT owned by any application (including this one).
+        VJD_STAT_BUSY, // The vJoy Device is owned by another application.
+        // It cannot be acquired by this application.
+        VJD_STAT_MISS, // The vJoy Device is missing. It either does not exist or the driver is down.
+        VJD_STAT_UNKN, // Unknown
+    }
+
+    public static class vJoy
+    {
+        [DllImport("vJoyInterface.dll")]
+        public static extern bool vJoyEnabled();
+
+        [DllImport("vJoyInterface.dll")]
+        public static extern short GetvJoyVersion();
+        
+        [DllImport("vJoyInterface.dll")]
+        private static extern IntPtr GetvJoyProductString();
+
+        [DllImport("vJoyInterface.dll")]
+        private static extern IntPtr GetvJoyManufacturerString();
+
+        [DllImport("vJoyInterface.dll")]
+        private static extern IntPtr GetvJoySerialNumberString();
+
+        public static string getProductString()
+        {
+            vJoyEnabled();
+            return Marshal.PtrToStringAuto(GetvJoyProductString());
+        }
+
+
+
+
+    }
+}
